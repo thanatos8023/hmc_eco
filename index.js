2f663975fbb7b3360bd1b37c437bd81d2fddde60
Jaeho <lee.jaeho8023@gmail.com>
2018-12-27 10:57:38 +0900

    api connection updated
    v 1.0.0

--- index.js
+++ index.js
@@ -79,50 +79,32 @@ kakaoRouter.post('/', function (req, res) {
 
     // 응답 결과를 카카오 형식으로 변환해서 카카오 챗봇에 응답함
     // 1. 버튼 응답이 없는 일반 텍스트
-    if (apiResponseBody.keyboard == null && apiResponseBody.message.message_button == null) {
+    if (apiResponseBody.type == "simpleText") {
+
       responseBody = {
         "version": "2.0",
         "template": {
           "outputs": [
             {
               "simpleText": {
-                "text": apiResponseBody.message.text,
+                "text": apiResponseBody.text,
               }
             }
           ]
         }
       }
+
     }
-    // 2. Url 링크가 존재하는 응답
-    else if (apiResponseBody.message.message_button != null) {
-      responseBody = {
-        "version": "2.0",
-        "template": {
-          "outputs": [
-            {
-              "basicCard": {
-                "description": apiResponseBody.message.text,
-                "buttons": [
-                  {
-                    "action": "webLink",
-                    "label": apiResponseBody.message.message_button.label,
-                    "webLinkUrl": apiResponseBody.message.message_button.url,
-                  }
-                ]
-              }
-            }
-          ]
-        }
-      }
-    }
-    // 3. 연결 링크만 존재하는 응답
-    else {
+    // 2. 버튼이 존재하는 응답
+    else if (apiResponseBody.type == "messageButton") {
+
       var buttonList = [];
       for (var i = 0; i < apiResponseBody.keyboard.buttons.length; i++) {
         buttonList.push({
-          "action": "message",
-          "label": apiResponseBody.keyboard.buttons[i],
-          "messageText": apiResponseBody.keyboard.buttons[i],
+          "action": apiResponseBody.buttons[i].action,
+          "label": apiResponseBody.buttons[i].label,
+          "url": apiResponseBody.buttons[i].url,
+          "messageText": apiResponseBody.buttons[i].messageText,
         });
       }
 
@@ -132,14 +114,126 @@ kakaoRouter.post('/', function (req, res) {
           "outputs": [
             {
               "basicCard": {
-                "description": apiResponseBody.message.text,
+                "description": apiResponseBody.text,
                 "buttons": buttonList,
               }
             }
           ]
         }
       }
+
     }
+    // 3. 이미지가 존재하는 버튼
+    else if (apiResponseBody.type == "imageButton") {
+
+      var buttonList = [];
+      for (var i = 0; i < apiResponseBody.buttons.length; i++) {
+        buttonList.push({
+          "action": apiResponseBody.buttons[i].action,
+          "label": apiResponseBody.buttons[i].label,
+          "url": apiResponseBody.buttons[i].url,
+          "messageText": apiResponseBody.buttons[i].messageText,
+        });
+      }
+
+      responseBody = {
+        "version": "2.0", 
+        "template": {
+          "outputs": [
+            {
+              "basicCard": {
+                "description": apiResponseBody.text,
+                "thumbnail": {
+                  "imageUrl": apiResponseBody.imageUrl,
+                }
+                "buttons": buttonList,
+              }
+            }
+          ]
+        }
+      }
+
+    }
+    // 4. 바로가기 연결 
+    // 카카오톡에서만 확인된 기능
+    // 네이버, 페이스북에서는 테스트가 필요함
+    else if (apiResponseBody.type == "quickReply") {
+
+      var quickList = [];
+      for (var i = 0; i < apiResponseBody.quickReplies.length; i++) {
+        quickList.push ({
+          "action": apiResponseBody.quickReplies[i].action,
+          "label": apiResponseBody.quickReplies[i].label,
+          "messageText": apiResponseBody.quickReplies[i].messageText,
+        });
+      }
+
+      responseBody = {
+        "version": "2.0",
+        "template": {
+          "outputs": [
+            {
+              "simpleText": {
+                "text": apiResponseBody.text,
+              }
+            }
+          ],
+          "quickReplies": quickList,
+        }
+      }
+
+    }
+    // 5. 리스트형
+    else if (apiResponseBody.type == "list") {
+
+      var itemList = [];
+      for (var i = 0; i < apiResponseBody.items.length; i++) {
+        itemList.push ({
+          "title": apiResponseBody.items[i].title,
+          "description": apiResponseBody.items[i].description,
+          "imageUrl": apiResponseBody.items[i].imageUrl,
+          "link": {
+            "web": apiResponseBody.items[i].homepage
+          }
+        });
+      }
+
+      responseBody = {
+        "version": "2.0",
+        "template": {
+          "outputs": [
+            {
+              "listCard": {
+                "header": {
+                  "title": apiResponseBody.text
+                },
+                "items": itemList,
+              }
+            }
+          ],
+        }
+      }
+
+    }
+    // 6. 이미지
+    else if (apiResponseBody.type == "image") {
+
+      responseBody = {
+        "version": "2.0",
+        "template": {
+          "outputs": [
+            {
+              "simpleImage": {
+                "imageUrl": 
+                "altText": "이미지를 찾을 수 없습니다.",
+              }
+            }
+          ],
+        }
+      }
+
+    }
+
 
     console.log("SERVER :: Kakao Eco :: Kakao response data");
     console.log(responseBody.template.outputs);
