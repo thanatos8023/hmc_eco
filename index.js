f4016e00ac06831664491afaabe0a87499f82e54
thanatos8023 <thanatos8023@gmail.com>
2019-02-11 15:42:40 +0900

    bugfix

--- index.js
+++ index.js
@@ -664,15 +664,15 @@ function callSendAPI (sender_psid, response) {
 const PAGE_ACCESS_TOKEN = 'EAALh6iqMeHoBAJH5scsmKvWBHZB2KY8ZBvNh1uSgQqJnCcga0cne1n4KrtD0drAQvYYW9vFZAVEAHNW5ZClvdEJvEPefkz9Crt8LvaJ0GQ7ZCYUSAPn2cbNziFEZC0B1vPiYGK8lH9Rtb6jrx9jQJ8ZBDClvb8MBi8aHcwugen3qgZDZD';
 const VERIFY_TOKEN = "VERIFY_TOKEN";
 
-facebookRouter.get('/webhook', function (req, res) {
-  console.log(req.query['hub.verify token']);
-  if (req.query['hub.verify token'] === VERIFY_TOKEN) {
-    return res.send(req.query['hub.challenge'])
+facebookRouter.get('/', function (req, res) {
+  console.log(req.query.hub.verify token);
+  if (req.query.hub.verify token === VERIFY_TOKEN) {
+    return res.send(req.query.hub.challenge)
   }
   res.send("wrong token");
 });
 
-facebookRouter.post('/webhook', function (req, res) {
+facebookRouter.post('/', function (req, res) {
   let body = req.body;
   if (body.object === 'page') {
     body.entry.forEach(function (entry) {
